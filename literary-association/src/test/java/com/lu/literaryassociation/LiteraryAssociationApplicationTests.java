046308374a068343f309b6329a9e542149747c5f
abeker <acabeker@gmail.com>
2020-11-28 16:07:25 +0100

    First commit

---
+++ literary-association/src/test/java/com/lu/literaryassociation/LiteraryAssociationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lu.literaryassociation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraryAssociationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
