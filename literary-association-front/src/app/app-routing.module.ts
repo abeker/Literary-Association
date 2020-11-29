96be94e94231bd41af6a3e2f8f90e1696fe71bab
jelenadragisic <jelenaa.dragisic@gmail.com>
2020-11-29 23:38:36 +0100

    login form

--- literary-association-front/src/app/app-routing.module.ts
+++ literary-association-front/src/app/app-routing.module.ts
@@ -1,14 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
-   {path:'', component: LoginComponent}
+  { path: '', pathMatch: 'full', redirectTo: '/auth/login' },
+  { path: 'auth/login', component: LoginComponent },
+  {
+    path: 'dashboard', component: DashboardComponent, children: [
+
+    ]
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
-
 export class AppRoutingModule { }
