78c631a937b89dc0f819a773aacf2e308affbd02
abeker <abeker@users.noreply.github.com>
2020-12-04 22:47:32 +0100

    registration back

---
+++ literary-association/src/main/java/com/lu/literaryassociation/dto/request/ReaderRegistration.java
@@ -0,0 +1,19 @@
+package com.lu.literaryassociation.dto.request;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class ReaderRegistration {
+
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String country;
+    private boolean betaReader;
+
+}
--- literary-association/src/main/java/com/lu/literaryassociation/services/implementation/ReaderService.java
+++ literary-association/src/main/java/com/lu/literaryassociation/services/implementation/ReaderService.java
@@ -1,8 +1,22 @@
 package com.lu.literaryassociation.services.implementation;
 
+import com.lu.literaryassociation.dto.request.ReaderRegistration;
+import com.lu.literaryassociation.dto.response.CreatedReader;
 import com.lu.literaryassociation.services.definition.IReaderService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReaderService implements IReaderService {
+
+    @Override
+    public CreatedReader registration(ReaderRegistration request) {
+        // TODO Povezati sa Camundom i odraditi proces
+
+        return CreatedReader.builder()
+                .email(request.getEmail())
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .username(request.getUsername()).build();
+    }
+
 }
